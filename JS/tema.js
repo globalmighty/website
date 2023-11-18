7aff6b5265b7051cf361640c567ca66ac67420e8
gatavares <joseserafimtavares89@gmail.com>
2023-11-18 00:53:02 +0000

    ajuste nos temas

--- JS/tema.js
+++ JS/tema.js
@@ -4,10 +4,14 @@ function changeTema() {
         tema = 'dark'
         document.getElementsByTagName("body")[0].classList.remove('light')
         document.getElementsByTagName("body")[0].classList.add('dark')
+        document.getElementById("logoLight").classList.add('hiden')
+        document.getElementById("logoDark").classList.remove('hiden')
     }
     else {
         tema = 'light'
         document.getElementsByTagName("body")[0].classList.remove('dark')
         document.getElementsByTagName("body")[0].classList.add('light')
+        document.getElementById("logoDark").classList.add('hiden')
+        document.getElementById("logoLight").classList.remove('hiden')
     }
 } 
