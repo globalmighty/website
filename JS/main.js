1a7da7fb268395e0c4addb563d7ca9c825e1f2bd
Guilherme Tavares <joseserafimtavares89@gmail.com>
2023-11-18 01:20:24 +0000

    Correçoes JS

--- JS/main.js
+++ JS/main.js
@@ -5,15 +5,15 @@
 let lastPage = 'Inicio'
 function getPoint(id) {
     if (id.slice(1) != lastPage) {
-
-        console.log(id.slice(1))
         document.getElementById(id.slice(1) + 'Nav').style.color = '#B8E4FE'
         document.getElementById('dot' + id.slice(1)).classList.remove('hiden');
+
         document.getElementById(lastPage + 'Nav').style.color = '#ffffff'
         document.getElementById('dot' + lastPage).classList.add('hiden');
+
         lastPage = id.slice(1)
     }
-    document.querySelector(id + 'Section').scrollIntoView({
+    document.querySelector(id).scrollIntoView({
         behavior: 'smooth'
     });
 }
