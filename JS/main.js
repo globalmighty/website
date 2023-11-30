7dc3f47aa19dc8da3685e4173d0796035c96355b
Guilherme Tavares <joseserafimtavares89@gmail.com>
2023-11-30 02:10:36 +0000

    80% update

--- JS/main.js
+++ JS/main.js
@@ -3,9 +3,9 @@ const percentage = document.querySelector('.progressText span')
 
 let per = 0;
 function progressLoad(){
-    if(per>=50){
-        progress.style.width = `50%`;
-        percentage.innerHTML = "50%"
+    if(per>=80){
+        progress.style.width = `80%`;
+        percentage.innerHTML = "80%"
 
     }else{
         progress.style.width = `${per}%`;
@@ -15,7 +15,7 @@ function progressLoad(){
     per++
 }
 
-setInterval(progressLoad,60)
+setInterval(progressLoad,80)
 
 const data = new Date();
 let ano = data.getFullYear();
